ebfb5e2b2e9d5c8ca887bbae825e1f3faefb7e23
kvintessen <threedaysgraccegor@gmail.com>
2026-08-16 22:00:35 +0300

    chore: rename app to TrustCart

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'TrustCart',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'container' => [
